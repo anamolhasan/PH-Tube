2bd56fe1dcdd074013ff73afc8de61764e87a9d9
Anamul Hassan <mdanamolhasan1998@gmail.com>
2025-03-12 07:18:03 +0600

    card show complite

--- script/index.js
+++ script/index.js
@@ -13,7 +13,12 @@ fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
 function loadVideos(){
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => displayvideos(data.videos))
+}
+
+const loadCategoriesVideos =(id)=>{
+   const url = `https://openapi.programming-hero.com/api/phero-tube/categories/${id}`
+   console.log(url)
 }
 
 // category : "Music"
@@ -27,7 +32,7 @@ for (const cat of categories) {
     // create Element
     const categoryDiv = document.createElement('div')
     categoryDiv.innerHTML = `
-     <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+     <button onclick='loadCategoriesVideos(${cat.category_id})' class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
     `
     //Append the Element
     categoryContainer.appendChild(categoryDiv)
@@ -35,6 +40,39 @@ for (const cat of categories) {
 }
 
 
+const displayvideos = (videos)=>{
+   const videoContainer = document.getElementById('video-container')
+   videos.forEach(video => {
+    console.log(video)
+    const videoCard = document.createElement('div')
+    videoCard.innerHTML = `
+    <div class="card bg-base-100 ">
+            <figure class="relative">
+              <img class='w-full h-[150px] object-cover' src= "${video.thumbnail}"
+                alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2">3hrs 56 min ago</span>
+            </figure>
+            <div class=" flex gap-3 px-0 py-5">
+             <div class="profile">
+                <div class="avatar">
+                    <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                      <img src="${video.authors[0].profile_picture
+                      }" />
+                    </div>
+                  </div>
+             </div>
+             <div class="intro">
+                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} 
+                <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">
+                </p>
+                <p class="text-sm text-gray-400">${video.others.views} views</p>
+             </div>
+            </div>
+          </div>
+    `
+    videoContainer.appendChild(videoCard)
+   });
+}
 
 loadCategories()
-loadVideos()
